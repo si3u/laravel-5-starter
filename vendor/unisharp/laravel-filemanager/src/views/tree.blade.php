a04683e2a3826b81d962935c4df48e129dc482e7
wicod <roth.nicolas6@gmail.com>
2017-10-10 11:49:53 +0200

    update

--- vendor/unisharp/laravel-filemanager/src/views/tree.blade.php
+++ vendor/unisharp/laravel-filemanager/src/views/tree.blade.php
@@ -1,4 +1,11 @@
 <ul class="list-unstyled">
+	
+	@if(request('dir'))
+	<li>COUCOU : {{  request('dir') }}</li>
+	@endif
+	
+{{--  Pour activer le dossier shares, ça se passe dans Traits/LfmHelpers.php @method getFormatedWorkingDir()  --}}
+
   @foreach($root_folders as $root_folder)
     <li>
       <a class="clickable folder-item" data-id="{{ $root_folder->path }}">
